e138078f7593c7fc90451ef3f73c7ca96cdae1b3
Sarthak Sen <sarthak.sen.01@gmail.com>
2021-07-11 01:23:38 +0530

    authentication

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyD1n29kQ8hFt1xFqrReo6WaDfUrt8rbdtc',
   authDomain: 'chat-web-app-d7bd8.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-d7bd8-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-web-app-d7bd8',
   storageBucket: 'chat-web-app-d7bd8.appspot.com',
   messagingSenderId: '198512902087',
@@ -11,3 +15,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Panel, Row, Col, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
-  return <div>Sigh In</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+      Alert.success('Signed In', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  const onStrangerSignIn = () => {
+    signInWithProvider(new firebase.auth.EmailAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chirp</h2>
+                <p>A Progressive Chat Platform</p>
+              </div>
+
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+                <Button block color="red" onClick={onStrangerSignIn}>
+                  <Icon icon="envelope" /> Continue with Email
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;

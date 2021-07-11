c4d1120cb5fbb7d720cb3d8707d244d91748a9bb
Sarthak Sen <sarthak.sen.01@gmail.com>
2021-07-11 14:06:02 +0530

    user profile with context, put real time subs

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = false;
-  if (profile) {
-    return <Redirect to="/" />;
+  const { isLoading, profile } = useProfile();
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!isLoading && !profile) {
+    return <Redirect to="/signin" />;
   }
   return <Route {...routeProps}>{children}</Route>;
 };
